a106651f49587045521bbfb977517691bf704e3a
Dayvid0063 <orjidavid63@gmail.com>
2024-06-14 00:18:34 +0100

    task 7 new

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,3 +1,5 @@
+-- Creates a stored procedure ComputeAverageScoreForUser that
+-- computes and store the average score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
