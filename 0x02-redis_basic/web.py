fc175af331a15032c59e4854d1b5bdc12065e9b2
Dayvid0063 <orjidavid63@gmail.com>
2024-06-19 17:27:35 +0100

    web task update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,36 +11,24 @@ from typing import Callable
 cl = redis.Redis()
 
 
-def count(method: Callable) -> Callable:
-    """Count the times a method is called"""
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        """Wrapper func for the method"""
-        key = f"count:{url}"
-        cl.incr(key)
-        return method(url)
-    return wrapper
-
-
 def cache(method: Callable) -> Callable:
     """Cache the result of a method in Redis"""
     @wraps(method)
-    def wrapper(url: str) -> str:
+    def wrapper(url) -> str:
         """Wrapper func for the method"""
-        key = f"page:{url}"
-        res = cl.get(key)
-        if res:
-            return res.decode('utf-8')
-
+        cl.incr(f'count:{url}')
+        result = cl.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
         result = method(url)
-        cl.setex(key, 10, result)
+        cl.set(f'count:{url}', 0)
+        cl.setex(f'result:{url}', 10, result)
         return result
     return wrapper
 
 
-@count
 @cache
 def get_page(url: str) -> str:
     """Gets the HTML content of a URL"""
-    response = requests.get(url)
-    return response.text
+    res = requests.get(url)
+    return res.text
