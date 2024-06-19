2a2394ea527f14a4a50f218435393396c00c8ca2
Dayvid0063 <orjidavid63@gmail.com>
2024-06-19 18:06:32 +0100

    web test 26

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python3
-"""Tools for request caching and tracking"""
+"""Request caching and tracking"""
 
 
-import redis
 import requests
-from functools import wraps
-from typing import Callable
+import redis
 
 
-redis_store = redis.Redis()
-"""The module-level Redis instance"""
+redis_client = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    """Caches the output of fetched data"""
-    @wraps(method)
-    def invoker(url) -> str:
-        """Wrapper func for caching output"""
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
 def get_page(url: str) -> str:
-    """Returns the content of a URL after caching the request"""
-    return requests.get(url).text
+    """Gets the HTML content and caches it with expiry"""
+    count_key = f"count:{url}"
+    redis_client.incr(count_key)
+
+    response = requests.get(url)
+    hc = response.text
+
+    cache_key = f"content:{url}"
+    redis_client.setex(cache_key, 10, hc)
+
+    return hc
