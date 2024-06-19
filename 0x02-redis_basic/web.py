52e891548057287294a2ce67627b28cae95b98c1
Dayvid0063 <orjidavid63@gmail.com>
2024-06-19 18:42:03 +0100

    web test 38

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,22 +6,18 @@ import requests
 import redis
 
 
-redis_client = redis.Redis()
+cli = redis.Redis()
 
 
 def get_page(url: str) -> str:
     """Gets the HTML content and caches it with expiry"""
     count_key = f"count:{url}"
-    redis_client.incr(count_key)
+    cli.incr(count_key)
+
+    response = requests.get(url)
+    hc = response.text
 
     cache_key = f"content:{url}"
-    cc = redis_client.get(cache_key)
-    if cc:
-        return cc.decode('utf-8')
-
-    response = requests.get(
-        f"http://slowwly.robertomurray.co.uk/delay/1000/url/{url}")
-    hc = response.text
-    redis_client.setex(cache_key, 10, hc)
+    cli.setex(cache_key, 10, hc)
 
     return hc
